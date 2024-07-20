9c78d979b7ee500ae6a88f29ee9ddbad30b182cb
Oliver Bourne <otgb97@gmail.com>
2024-07-20 23:08:45 +0100

    Initial project

---
+++ TradingVisualizerApi/Controllers/TradesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradingVisualizerApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TradesController : ControllerBase
+{
+}
---
+++ TradingVisualizerModels/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TradingVisualizerModels
+{
+    public class Class1
+    {
+
+    }
+}
